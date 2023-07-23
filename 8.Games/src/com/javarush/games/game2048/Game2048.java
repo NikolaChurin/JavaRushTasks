8463c5a91b9448443f672bc0f4f70bd9242b32d9
NYChurin <i.love.pizza.rus@gmail.com>
2023-07-23 21:39:31 +0300

    Add solution java game 2048 part 5-18

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -14,18 +14,25 @@ public class Game2048 extends Game {
     }
 
     private void createGame() {
-
+        createNewNumber();
+        createNewNumber();
     }
 
     private void drawScene() {
         for (int i = 0; i < SIDE; i++) {
             for (int j = 0; j < SIDE; j++) {
-                setCellColor(i,j,Color.PURPLE);
+                setCellColor(i, j, Color.PURPLE);
             }
         }
-
     }
 
-
+    private void createNewNumber() {
+        int x = getRandomNumber(SIDE);
+        int y = getRandomNumber(SIDE);
+        if (gameField[x][y] == 0) {
+            int n = getRandomNumber(10);
+            gameField[x][y] = n < 9 ? 2 : 4;
+        } else createNewNumber();
+    }
 }
 
