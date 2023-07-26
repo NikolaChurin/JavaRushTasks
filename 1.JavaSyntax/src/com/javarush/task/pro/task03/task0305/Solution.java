878a57ea0ce89033fb35aa6a0b38673d4d377aa1
NYChurin <i.love.pizza.rus@gmail.com>
2023-07-26 21:09:21 +0300

    Add solution java tasks

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0305/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task03.task0305;
+
+import java.util.Scanner;
+
+/* 
+Три числа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+        if (num1 == num2 && num2 == num3)
+            System.out.println(num1 + " " + num2 + " " + num3);
+        else {
+            if (num1 == num2) System.out.println(num1 + " " + num2);
+            if (num1 == num3) System.out.println(num1 + " " + num3);
+            if (num2 == num3) System.out.println(num2 + " " + num3);
+        }
+    }
+}
