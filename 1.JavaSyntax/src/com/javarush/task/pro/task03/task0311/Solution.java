970bcc78ba24c8a6372c9036df698a152f7c147e
NYChurin <i.love.pizza.rus@gmail.com>
2023-07-30 13:20:46 +0300

    Add solution java task 0309, 0310, 0311

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0311/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task03.task0311;
+
+import java.util.Scanner;
+
+/* 
+Высокая точность
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        String out = Math.abs(a-b)<0.000001 ? "числа равны" : "числа не равны";
+        System.out.println(out);
+
+    }
+}
