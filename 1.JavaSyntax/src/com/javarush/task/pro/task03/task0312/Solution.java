82f384c2d0887183c9f97b763cc086f1df0192a3
NYChurin <i.love.pizza.rus@gmail.com>
2023-08-03 19:02:46 +0300

    Add solution tasks 312, 313, 314

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0312/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task03.task0312;
+
+import java.util.Scanner;
+
+/* 
+Сравним строки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String first = scanner.nextLine();
+        String second = scanner.nextLine();
+        if (first.equals(second))
+            System.out.println("строки одинаковые");
+        else
+            System.out.println("строки разные");
+    }
+}
