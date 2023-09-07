47d0c6350451e2a530f30f608476f1755d6bf51e
NYChurin <i.love.pizza.rus@gmail.com>
2023-09-07 09:04:10 +0300

    Add solution task 0404

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task04.task0404;
+
+/* 
+Заполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int b =0;
+        while (b<5){
+            int a=0;
+            while (a<10){
+                System.out.print("Q");
+                a++;
+            }
+            System.out.println();
+            b++;
+        }
+    }
+}
