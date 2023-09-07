476593b9e3b0e172aae3cba2a011b4cc363f8633
NYChurin <i.love.pizza.rus@gmail.com>
2023-09-07 09:04:51 +0300

    Add solution task 0406

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0406/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task04.task0406;
+
+import java.util.Scanner;
+
+/* 
+Показываем, что получаем
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String i = scanner.nextLine();
+            if (i.equals("enough")) break;
+            System.out.println(i);
+        }
+    }
+}
