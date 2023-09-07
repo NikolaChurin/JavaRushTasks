2ca2f8f736721b583054c77ce9539bdff4c9ebfe
NYChurin <i.love.pizza.rus@gmail.com>
2023-09-07 11:35:45 +0300

    Add solution task 0412

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0410/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0410/Solution.java
@@ -8,7 +8,21 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        Scanner scanner = new Scanner(System.in);
+        int a = 2;
+        int b = 2;
+        int i = 0;
+        while (scanner.hasNextInt()) {
+            if (i < 1) a = b = scanner.nextInt();
+            i++;
+            int n = scanner.nextInt();
+            if (a == b && n > b) b = n;
+            if (n < b && n < a) {
+                b = a;
+                a = n;
+            }
+            if (n < b && n > a) b = n;
+        }
+        System.out.println(b);
     }
 }
