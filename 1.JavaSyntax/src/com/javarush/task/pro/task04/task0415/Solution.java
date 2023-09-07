3a89f93a28ad83037aa5cac50d7f1921f728d9fd
NYChurin <i.love.pizza.rus@gmail.com>
2023-09-07 09:07:23 +0300

    Add solution task 0415

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0415/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task04.task0415;
+
+import java.util.Scanner;
+
+/* 
+Площадь круга
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      double pi = 3.14;
+      double radius = scanner.nextDouble();
+      int s =(int) (pi * radius * radius);
+        System.out.println(s);
+    }
+}
