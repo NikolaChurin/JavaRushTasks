fcb61603c63d4c86c4bb0a43110c468581404445
NYChurin <i.love.pizza.rus@gmail.com>
2023-09-07 08:58:29 +0300

    Add solution task 0401

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task04.task0401;
+
+/* 
+Я никогда не буду работать за копейки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+        int s=0;
+        while (s<100){
+            System.out.println(quote);
+            s++;
+        }
+
+    }
+}
