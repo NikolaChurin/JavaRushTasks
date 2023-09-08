abf5557d0cdf8a294f64239ac4fb458d2a8945b0
NYChurin <i.love.pizza.rus@gmail.com>
2023-09-08 17:43:34 +0300

    Add solution task 0507

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0507/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task05.task0507;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/* 
+Максимальное из N чисел
+*/
+
+public class Solution {
+    public static int[] array;
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        Arrays.sort(array);
+        System.out.println(array[array.length-1]);
+    }
+}
