05c22b6bc547eea7ca12118b75b4fc84c56b440a
NYChurin <i.love.pizza.rus@gmail.com>
2023-09-08 09:00:07 +0300

    Add solution task 0413

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0413/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0413/Solution.java
@@ -6,7 +6,11 @@ package com.javarush.task.pro.task04.task0413;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        for (int i = 1; i < 11 ; i++) {
+            for (int j = 0; j < i ; j++) {
+                System.out.print(8);
+            }
+            System.out.println();
+        }
     }
 }
