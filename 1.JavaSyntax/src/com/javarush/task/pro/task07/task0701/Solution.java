aa7fc445ef06188cc158af2b86d415b06f1bb5e4
NYChurin <i.love.pizza.rus@gmail.com>
2024-01-13 01:13:49 +0300

    Add solution task 0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    byte a;
+    char a1;
+    short b;
+    int c;
+    long d;
+
+    float e;
+    double f;
+
+    boolean g;
+}
