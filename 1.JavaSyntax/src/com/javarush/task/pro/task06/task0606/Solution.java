9894f858e88114c7114ee2e7d35db4e947cbf1cd
NYChurin <i.love.pizza.rus@gmail.com>
2024-01-13 01:09:03 +0300

    Add solution task 0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+    }
+    public static void universalMethod(int a){
+    }
+    public static void universalMethod(int a, boolean yes){
+    }
+    public static void universalMethod(short a){
+    }
+    public static void universalMethod(long a){
+    }
+    public static void universalMethod(byte a){
+    }
+    public static void universalMethod(byte a, boolean yes){
+    }
+    public static void universalMethod(short a, boolean yes){
+    }
+    public static void universalMethod(long a, boolean yes){
+    }
+    public static void universalMethod(int a, int x){
+    }
+}
